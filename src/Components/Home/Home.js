6fe52b3664f0c469506d8da3afb3b05046d4f533
shihabhassannaim <hassanshihab231@gmail.com>
2023-07-24 16:02:49 +0600

    1st

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+
+const Home = () => {
+    const[post , setPost] = useState(0);
+    useEffect(()=> {
+        
+    } , [])
+    return (
+        <div>
+            <h1>This is Home </h1>
+        </div>
+    );
+};
+
+export default Home;
